7ecea03b3bbec1ab7e38f6605541c25394dc410e
JangHyeok Kim <48301140+d0orHyeok@users.noreply.github.com>
2021-11-05 13:22:14 +0900

    Add: devDependencies prettier & eslint

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
-const path = require("path");
-const config = require("./config/key");
+const path = require('path');
+const config = require('./config/key');
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const connect = mongoose
-    .connect(config.mongoURI)
-    .then(() => console.log("MongoDB Connected..."))
-    .catch((err) => console.log(err));
+  .connect(config.mongoURI)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 app.use(cors());
 
@@ -22,22 +22,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/users", require("./routes/users"));
+app.use('/api/users', require('./routes/users'));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-    // Set static folder
-    // All the javascript and css files will be read and served from this folder
-    app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static('client/build'));
 
-    // index.html for all page routes    html or routing and naviagtion
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-    });
+  // index.html for all page routes    html or routing and naviagtion
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
 }
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server Listening on ${port}`);
+  console.log(`Server Listening on ${port}`);
 });
