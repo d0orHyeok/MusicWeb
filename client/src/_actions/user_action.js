80b8e7d2d45388a3c7219be11575b2f13c9bc2d6
JangHyeok Kim <48301140+d0orHyeok@users.noreply.github.com>
2021-11-06 17:58:12 +0900

    Add: redux

---
+++ client/src/_actions/user_action.js
@@ -0,0 +1,39 @@
+import Axios from 'axios';
+import { USER_SERVER } from '../Config';
+import { LOGIN_USER, REGISTER_USER, LOGOUT_USER, AUTH_USER } from './types';
+
+export function registerUser(data) {
+    const req = Axios.post(`${USER_SERVER}/register`, data).then(res => (res = res.data));
+
+    return {
+        type: REGISTER_USER,
+        payload: req,
+    };
+}
+
+export function loginUser(data) {
+    const req = Axios.post(`${USER_SERVER}/login`, data).then(res => (res = res.data));
+
+    return {
+        type: LOGIN_USER,
+        payload: req,
+    };
+}
+
+export function logoutUser(data) {
+    const req = Axios.get(`${USER_SERVER}/login`, data).then(res => (res = res.data));
+
+    return {
+        type: LOGOUT_USER,
+        payload: req,
+    };
+}
+
+export function auth(data) {
+    const req = Axios.get(`${USER_SERVER}/login`, data).then(res => (res = res.data));
+
+    return {
+        type: AUTH_USER,
+        payload: req,
+    };
+}
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider
+            store={createStoreWithMiddleware(
+                Reducer,
+                // For redux devtools
+                window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+            )}
+        >
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 reportWebVitals();
